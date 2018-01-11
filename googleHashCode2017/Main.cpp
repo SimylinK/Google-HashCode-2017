26d2c31ff4e8b85b7e801e98cf36b110e11919a9
Lucas Pagano <lucas.pagano@etu.univ-nantes.fr>
2018-01-11 14:17:41 +0100

    change to take input file as argument instead of hardcoding it

--- googleHashCode2017/Main.cpp
+++ googleHashCode2017/Main.cpp
@@ -5,28 +5,32 @@
 
 #include "Output.h"
 
-int main() {
-	Plan p("../inputData/opera.in");
+int main(int argc, char *argv[]) {
 
-	std::cout << "MaxBudget : " << p.getMaxBudget() << std::endl;
-	std::cout << "Router cost " << p.getRouterCost() << std::endl;
-	std::cout << "Wire cost " << p.getWireCost() << std::endl;
-	solveProblem(p);
+    if(argc == 2) {
+        std::cout << argv[1] << std::endl;
+        Plan p(argv[1]);
 
-	std::cout << "money used : " << p.getSpentMoney() << std::endl;
+        std::cout << "MaxBudget : " << p.getMaxBudget() << std::endl;
+        std::cout << "Router cost : " << p.getRouterCost() << std::endl;
+        std::cout << "Wire cost : " << p.getWireCost() << std::endl;
+        solveProblem(p);
 
-	// To fill an output file (in notepadd++ you can dezoom to analyse big maps)
-	std::ofstream file("../output/test.out", std::ios::out);
-	if (file) {
-		file << p << std::endl;
-	}
+        std::cout << "money used : " << p.getSpentMoney() << std::endl;
 
-	try {
-		output(p, "../outputFile.out");
-	}
-	catch (std::exception e) {
-		std::cout << e.what();
-	}
+        // To fill an output file (in notepadd++ you can dezoom to analyse big maps)
+        std::ofstream file("../output/test.out", std::ios::out);
+        if (file) {
+            file << p << std::endl;
+        }
+
+        try {
+            output(p, "../outputFile.out");
+        }
+        catch (std::exception e) {
+            std::cout << e.what();
+        }
+    }
 
 	return 0;
 }
